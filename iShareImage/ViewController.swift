c3a2c64b23478d65961535fe764923c4508373cd
Juan Ml. Morillo <juanmorillios@gmail.com>
2015-08-30 15:53:24 +0200

    Initial Commit

---
+++ iShareImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iShareImage
+//
+//  Created by Juan Morillo on 30/8/15.
+//  Copyright (c) 2015 JuanMorillios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iShareImageTests/iShareImageTests.swift
@@ -0,0 +1,36 @@
+//
+//  iShareImageTests.swift
+//  iShareImageTests
+//
+//  Created by Juan Morillo on 30/8/15.
+//  Copyright (c) 2015 JuanMorillios. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class iShareImageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
